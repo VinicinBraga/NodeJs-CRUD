b24beadd0197c7058e632ce6037ee487eeeb7be9
VinicinBraga <vinicius@bragamatta.com.br>
2021-09-27 11:17:48 -0300

    Crud simples

--- index.js
+++ index.js
@@ -3,11 +3,43 @@ const server = express();
 const port = 3000;
 server.use(express.json());
 
-const skills = ["Html", "Css", "Js", "NodeJs", "Python"];
+const skills = ["html", "css", "Js", "NodeJs", "Python"];
 
-server.get("/skills/:index", (req, res) => {
-  const { index } = req.params;
-  console.log(skills[index]);
+//Retorna uma skill
+server.get("/skills/:i", (req, res) => {
+  const { i } = req.params;
+  return res.json(skills[i]);
+});
+
+//Create
+server.post("/skills/", (req, res) => {
+  const { name } = req.body;
+  skills.push(name);
+
+  return res.json(skills);
+});
+
+//Read
+server.get("/skills/", (req, res) => {
+  return res.json(skills);
+});
+
+// Update
+server.put("/skills/:i", (req, res) => {
+  const { i } = req.params;
+  const { name } = req.body;
+
+  skills[i] = name;
+
+  return res.json(skills);
+});
+
+//Delete
+server.delete("/skills/:i", (req, res) => {
+  const { i } = req.params;
+
+  skills.splice(i, 1);
+  return res.json({ message: `the skills ${i} was removed` });
 });
 
 server.listen(port, () => console.log(`Example app listening on port port!`));
